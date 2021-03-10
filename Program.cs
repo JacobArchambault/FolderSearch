3f3f93922ac97c90431630c1c730deb6a76b6def
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-09 22:36:00 -0500

    Add StartDirectory class from which to get files.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.IO;
 namespace FolderSearch
 {
     class Program
     {
         static void Main(string[] args)
         {
+            new StartDirectory(new DirectoryInfo("../../../input")).CopyFrom();
             Console.WriteLine("Hello World!");
         }
     }
---
+++ StartDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSearch
+{
+    class StartDirectory
+    {
+        private readonly DirectoryInfo directoryInfo;
+        public StartDirectory(DirectoryInfo directoryInfo)
+        {
+            this.directoryInfo = directoryInfo;
+        }
+
+        internal void CopyFrom()
+        {
+            try
+            {
+                directoryInfo.GetFiles();
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+    }
+}
