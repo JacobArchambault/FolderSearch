70c87d6779dd81ff775cc670228a96e26a01dc58
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-09 22:22:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FolderSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
