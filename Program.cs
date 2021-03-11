c46a22d76da412470564de27ecea54fb83f66c38
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-11 10:46:34 -0500

    Get start directory from Prompt.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,32 @@
 ﻿using System.IO;
+using System;
 namespace FolderSearch
 {
     class Program
     {
         static void Main()
         {
+            Console.WriteLine("Welcome to the file copier app");
             new FilesToCopy(
                 new DateFilteredFiles(
                     new RegexFilteredFiles(
                         new SortedDirectory(
                             new StartDirectory()))))
                 .CopyFilesRecursively(
-                new DirectoryInfo("../../../input"),
+                new DirectoryInfo(Prompt()),
                 new DirectoryInfo("../../../output"));
+        }
+
+        private static string Prompt()
+        {
+            Console.WriteLine("Please enter the directory path you want to copy files from");
+            var startDirectory = Console.ReadLine();
+            while (!Directory.Exists(startDirectory))
+            {
+                Console.WriteLine("That directory doesn't exist. Please enter another directory path");
+                startDirectory = Console.ReadLine();
+            }
+            return startDirectory;
         }
     }
 }
