e8d07dea6009fc6aade9b29751a82f944bc7f045
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-11 13:39:41 -0500

    Move prompt methods into their own static prompt class, separate main Program logic into its own Run method, and surround it with try-catch.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using FolderSearch.IDirectories;
 using System;
 using System.IO;
-
+using static FolderSearch.Prompt;
 namespace FolderSearch
 {
     class Program
@@ -9,6 +9,18 @@ namespace FolderSearch
         static void Main()
         {
             Console.WriteLine("Welcome to the file copier app");
+            try
+            {
+                Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void Run()
+        {
             new FilesToCopy(
                         new SortedDirectory(
                             new DateFilteredFiles(
@@ -23,38 +35,11 @@ namespace FolderSearch
                 new DirectoryInfo(
                     TextResponse(
                         "Please enter the directory path you want to copy files from: ",
-                        "That directory doesn't exist. Please enter another directory path")),
+                        "That directory doesn't exist. Please enter another directory path",
+                        Directory.Exists)),
                 new DirectoryInfo(
                     TextResponse(
                         "Enter the directory path you want to copy files to: ")));
-        }
-
-        private static string TextResponse(string toPrompt)
-        {
-            Console.Write(toPrompt);
-            return Console.ReadLine();
-        }
-
-        private static string TextResponse(string toPrompt, string withInvalidInputMessage)
-        {
-            Console.WriteLine(toPrompt);
-            var response = Console.ReadLine();
-            while (!Directory.Exists(response))
-            {
-                Console.WriteLine(withInvalidInputMessage);
-                response = Console.ReadLine();
-            }
-            return response;
-        }
-        private static int NumberResponse(string fromPrompt, string withErrorMessage)
-        {
-            Console.WriteLine(fromPrompt);
-            int response;
-            while (!int.TryParse(Console.ReadLine(), out response) || response < 0)
-            {
-                Console.WriteLine(withErrorMessage);
-            }
-            return response;
         }
     }
 }
---
+++ Prompt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSearch
+{
+    static class Prompt
+    {
+        internal static string TextResponse(string toPrompt)
+        {
+            Console.Write(toPrompt);
+            return Console.ReadLine();
+        }
+
+        internal static string TextResponse(string toPrompt, string withInvalidInputMessage, Func<string, bool> passCondition)
+        {
+            Console.WriteLine(toPrompt);
+            var response = Console.ReadLine();
+            while (!passCondition(response))
+            {
+                Console.WriteLine(withInvalidInputMessage);
+                response = Console.ReadLine();
+            }
+            return response;
+        }
+
+        internal static int NumberResponse(string fromPrompt, string withErrorMessage)
+        {
+            Console.WriteLine(fromPrompt);
+            int response;
+            while (!int.TryParse(Console.ReadLine(), out response) || response < 0)
+            {
+                Console.WriteLine(withErrorMessage);
+            }
+            return response;
+        }
+    }
+}
