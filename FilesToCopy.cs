3bb5ee2c1bb5a89fda64c515f97958f9f11b25e2
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2021-08-09 17:22:04 -0400

    Make MaxFiles.EnumerateFiles static

--- FilesToCopy.cs
+++ FilesToCopy.cs
@@ -1,14 +1,15 @@
 ﻿using FolderSearch.IFilesImplementations;
 using System.IO;
+using static FolderSearch.IFilesImplementations.MaxFiles;
 namespace FolderSearch
 {
     static class FilesToCopy
     {
-        public static void CopyRecursively(MaxFiles filteredFiles, int maxNumber, DirectoryInfo source, DirectoryInfo target)
+        public static void CopyRecursively(IFiles filteredFiles, int maxNumber, DirectoryInfo source, DirectoryInfo target)
         {
             foreach (DirectoryInfo dir in source.EnumerateDirectories())
                 CopyRecursively(filteredFiles, maxNumber, dir, target.CreateSubdirectory(dir.Name));
-            foreach (FileInfo file in filteredFiles.EnumerateFiles(source, maxNumber))
+            foreach (FileInfo file in EnumerateFiles(filteredFiles, source, maxNumber))
                 file.CopyTo(Path.Combine(target.FullName, file.Name), true);
         }
     }
--- IFilesImplementations/MaxFiles.cs
+++ IFilesImplementations/MaxFiles.cs
@@ -4,14 +4,9 @@ using System.Linq;
 
 namespace FolderSearch.IFilesImplementations
 {
-    class MaxFiles
+    static class MaxFiles
     {
-        readonly IFiles source;
-        internal MaxFiles(IFiles source)
-        {
-            this.source = source;
-        }
-        public IEnumerable<FileInfo> EnumerateFiles(DirectoryInfo directory, int maxNumber)
+        public static IEnumerable<FileInfo> EnumerateFiles(IFiles source, DirectoryInfo directory, int maxNumber)
         {
             return source.EnumerateFiles(directory).Take(maxNumber);
         }
