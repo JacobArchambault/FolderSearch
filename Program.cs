2a006334e6b45a137c09edd4fb4308a91dea8602
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2021-08-09 16:21:56 -0400

    Use FilesToCopy statically

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using static FolderSearch.Prompt;
+using static FolderSearch.FilesToCopy;
 namespace FolderSearch
 {
     class Program
@@ -21,26 +22,25 @@ namespace FolderSearch
 
         private static void Run()
         {
-            FilesToCopy
-                .CopyRecursively(
-                    new MaxFiles(
-                        new SortedDirectory(
-                            new DateFilteredFiles(
-                                new RegexFilteredFiles(
-                                    new StartDirectory(),
-                                    TextResponse(
-                                        "Enter text or a regex string for the file names you'd like to search for, or press enter to skip this step: ")),
-                                NumberResponse(
-                                    "From how many days ago would you like to keep files? "))),
-                        NumberResponse("How many files would you like to keep per folder? ")),
-                    new DirectoryInfo(
-                        TextResponse(
-                            "Please enter the directory path you want to copy files from: ",
-                            "That directory doesn't exist. Please enter another directory path",
-                            Directory.Exists)),
-                    new DirectoryInfo(
-                        TextResponse(
-                            "Enter the directory path you want to copy files to: ")));
+            CopyRecursively(
+                new MaxFiles(
+                    new SortedDirectory(
+                        new DateFilteredFiles(
+                            new RegexFilteredFiles(
+                                new StartDirectory(),
+                                TextResponse(
+                                    "Enter text or a regex string for the file names you'd like to search for, or press enter to skip this step: ")),
+                            NumberResponse(
+                                "From how many days ago would you like to keep files? "))),
+                    NumberResponse("How many files would you like to keep per folder? ")),
+                new DirectoryInfo(
+                    TextResponse(
+                        "Please enter the directory path you want to copy files from: ",
+                        "That directory doesn't exist. Please enter another directory path",
+                        Directory.Exists)),
+                new DirectoryInfo(
+                    TextResponse(
+                        "Enter the directory path you want to copy files to: ")));
         }
     }
 }
