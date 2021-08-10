453a2743c7182e26d43250a8fc4e0ced9ddb3947
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2021-08-10 14:13:44 -0400

    Delete SortedDirectory

--- FilesToCopy.cs
+++ FilesToCopy.cs
@@ -13,11 +13,11 @@ namespace FolderSearch
             foreach (DirectoryInfo dir in source.EnumerateDirectories())
                 CopyRecursively(regexFilter, maxNumber, cutOffDate, dir, target.CreateSubdirectory(dir.Name));
             foreach (FileInfo file in MaxFiles.EnumerateFiles(
-                                        SortedDirectory.EnumerateFiles(
-                                            source
-                                                .EnumerateFiles()
-                                                .Where(file => Regex.IsMatch(file.FullName, regexFilter))
-                                                .Where(file => file.LastWriteTime > DateTime.Today.Subtract(TimeSpan.FromDays(cutOffDate)))), 
+                                        source
+                                            .EnumerateFiles()
+                                            .Where(file => Regex.IsMatch(file.FullName, regexFilter))
+                                            .Where(file => file.LastWriteTime > DateTime.Today.Subtract(TimeSpan.FromDays(cutOffDate)))
+                                            .OrderByDescending(f => f.LastWriteTime), 
                                         maxNumber))
             {
                 file.CopyTo(Path.Combine(target.FullName, file.Name), true);
