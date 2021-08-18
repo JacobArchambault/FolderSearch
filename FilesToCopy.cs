4a49fc5a9c8c2f7f247ffc0e78385418333f4e8e
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2021-08-17 22:40:37 -0400

    Make FilterFiles take this parameter

--- FilesToCopy.cs
+++ FilesToCopy.cs
@@ -12,16 +12,15 @@ namespace FolderSearch
         {
             foreach (DirectoryInfo dir in source.EnumerateDirectories())
                 CopyRecursively(regexFilter, maxNumber, dir, target.CreateSubdirectory(dir.Name));
-            foreach (FileInfo file in FilterFiles(regexFilter, maxNumber, source))
+            foreach (FileInfo file in source.EnumerateFiles().FilterFiles(regexFilter, maxNumber, source))
             {
                 file.CopyTo(Path.Combine(target.FullName, file.Name), true);
             }
         }
 
-        private static IEnumerable<FileInfo> FilterFiles(Func<FileInfo, bool> filter, int maxNumber, DirectoryInfo source)
+        private static IEnumerable<FileInfo> FilterFiles(this IEnumerable<FileInfo> files, Func<FileInfo, bool> filter, int maxNumber, DirectoryInfo source)
         {
-            return source
-                .EnumerateFiles()
+            return files
                 .Where(filter)
                 .OrderByDescending(f => f.LastWriteTime)
                 .Take(maxNumber);
